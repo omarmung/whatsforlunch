269fd73d533d7df4b21636cf4941fe577d6c348b
Niels van Eck <niels@twitter.com>
2013-09-02 09:28:16 -0700

    upgrading to new manifest version, going popup only and using localstorage

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,62 +1,77 @@
 // takes xml and parses each xml item into html
+
+
 function parse_post(element) {
-	// console.log(element);
-	var post = new Object();
-	post.title = $(element).find("title").text();
-	// post.title = post.title.is_this_today() // wow... this is bad... going to sleep now
-	post.url = "";
-	post.description = $("<div/>").html($(element).find("description")).text();
-	// post.description = post.description.toUpperCase();
-	post.description = post.description.replace(/ \- \$/g,"");
-	post.description = post.description.replace(/<h4>/g,"<h5><span class='label success'>Free</span><span> </span>");
-	post.description = post.description.replace(/<\/h4>/g,"<\/h5>");
-	//console.log(post);
-	return post;
+  // console.log(element);
+  var post = new Object();
+  post.title = $(element).find("title").text();
+  // post.title = post.title.is_this_today() // wow... this is bad... going to sleep now
+  post.url = "";
+  post.description = $("<div/>").html($(element).find("description")).text();
+  // post.description = post.description.toUpperCase();
+  // post.description = post.description.replace(/ \- \$/g,"");
+  // post.meals = post.description.split('</p>');
+  post.description = post.description.replace(/<h4>\[/g, "<h3 class='hide'>");
+  post.description = post.description.replace(/\]/g, "</h3><h5><span class='label btn-success'>FREE</span><span> </span>");
+  // post.description = post.description.replace(/<h4>/g,"<h5><span class='label success'>Free</span><span> </span>");
+  post.description = post.description.replace(/<\/h4>/g, "<\/h5>");
+  // post.description = post.description.replace(/<p>/g,"<p class='hide'>"); // hiding on/off
+  post.description = post.description.replace(/<h3 class=\'hide\'>breakfast/, "<h3 class='show'>breakfast");
+  post.description = post.description.replace(/<h3 class=\'hide\'>\@birdfeeder/, "<h3 class='show'>@birdfeeder");
+  post.description = post.description.replace(/<h3 class=\'hide\'>\#tenderloin/, "<h3 class='show'>#tenderloin");
+  post.description = post.description.replace(/<h3 class=\'hide\'>\#comfort/, "<h3 class='show'>#comfort");
+  post.description = post.description.replace(/<h3 class=\'hide\'>\#sandwich/, "<h3 class='show'>#sandwich");
+  post.description = post.description.replace(/<h3 class=\'hide\'>\#salad/, "<h3 class='show'>#salad");
+  post.description = post.description.replace(/<h3 class=\'hide\'>soup/, "<h3 class='show'>soup");
+  post.description = post.description.replace(/<h3 class=\'hide\'>dinner/, "<h3 class='show'>dinner");
+  post.description = post.description.replace(/<h3 class=\'hide\'>global tea time/, "<h3 class='show'>global tea time");
+  post.description = post.description.replace(/<h3 class=\'hide\'>hack week snack/, "<h3 class='show'>hack week snack");
+  post.description = post.description.replace(/<h3 class=\'hide\'>happy hour/, "<h3 class='show'>happy hour =|;{>");
+
+
+
+/* post.meals = post.description.split(/\n/);
+		for (var i = 0; i < post.meals.length; i++) {
+		   post.meals = post.meals.push(post.meals.textContent[i]);
+		}
+	*/
+
+  //console.log(post);
+  return post;
 }
 
 
 // returns true if datetext is today, false if datetext !is today
-function is_datetext_today(datetext)
-{
- // today = new Date(); set in popup.html declarations			
-	someday = new Date(datetext);  // testing "Mon, Jan 30 2012"
-	today = new Date(); 
-	
-	if (someday.getDate() == today.getDate())
-	{
-		// $('#popup').append(today + "<br> = <br>" + someday);
-		return true; // someday.getDate() + "is Today: " + today.getDate();					
-	}
-	else
-	{
-		if (someday.getTime() > today.getTime())
-		{return true}
-		
-		// what we return when it's not today and not all these other things
-		// $('#popup').append(today + "<br> != <br>" + someday);
-		return false; // someday.getDate() + "is not Today: " + today.getDate();					
-	}
+
+
+function is_datetext_today(datetext) {
+  if (datetext == "Fri, 30 Aug 2013") return true;
+  
+  // today = new Date(); set in popup.html declarations			
+  someday = new Date(datetext); // testing "Mon, Jan 30 2012"
+  today = new Date();
+
+  if (someday.getDate() == today.getDate()) {
+    // $('#popup').append(today + "<br> = <br>" + someday);
+    return true; // someday.getDate() + "is Today: " + today.getDate();					
+  } else {
+    if (someday.getTime() > today.getTime()) {
+      return true
+    }
+
+    // what we return when it's not today and not all these other things
+    // $('#popup').append(today + "<br> != <br>" + someday);
+    return false; // someday.getDate() + "is not Today: " + today.getDate();					
+  }
 }
 
 
 
 function open_item(url) {
-	chrome.tabs.create({url: url});
-	chrome.browserAction.setBadgeText({text:''});
+  chrome.tabs.create({
+    url: url
+  });
+  chrome.browserAction.setBadgeText({
+    text: ''
+  });
 }
-
-
-// bullshit
-function bullshitnoconflict()
-{
-	var today = $(element).find("title").text("");
-}
-
-
-// bullshit i wrote before i went to sleep
-function is_this_todaynoconflict(txtdate)
-{
-		// set the badge background	
-		chrome.browserAction.setBadgeBackgroundColor({color:[0, 50, 0, 50]});		
-		return true;
-	  }
---
+++ scripts/popup.js
@@ -0,0 +1,119 @@
+var url = 'http://legacy.cafebonappetit.com/rss/menu/403';
+var lunchRss = "lunchRss";
+var lunchRssLastFetched = "lunchRssLastFetched";
+var lunchInfo = [lunchRss, lunchRssLastFetched];
+
+// Tell the background page to refetch now, 
+// for use when the regularly-scheduled feed didn't work
+// or network problems, etc.
+function re_fetch() {
+  ii = 0; // 0 is the first time through the loop, needed to clear the popup of any prior stuff
+  
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // set the RSS in local storage
+      chrome.storage.local.set({
+        lunchRss: xhr.responseText,
+        lunchRssLastFetched: new Date()
+      });
+    }
+  }
+  
+  // Note that any URL fetched here must be matched by a permission in
+  // the manifest.json file!
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+
+// Parse XML data passed back from the background page 
+function display_stories(lunchStorageResult, refetch) {
+  $xml = $(lunchStorageResult[lunchRss]);
+
+  var items = $xml.find("item");
+
+  //$('#popup').append("<hr><p>Is Today " + today + "</p><br> " + "<p>Someday? " + someday.getUTCDate() + "</p>"); // find_today());
+  // var days = $xml.find("title");
+  items.each(function(index, element) {
+
+    // grab the title text from each item (this is where date is kept)
+    today_title = is_datetext_today($(element).find("title").text());
+
+    // ask if the title matches today's date
+    if (today_title == true) {
+
+      // $('#popup').append(today_title);
+      // if (is_datetext_today( $(element).find("title").text() )
+      var post = parse_post(element);
+      var item = '';
+      var class2 = '';
+
+      if (index >= localStorage['unread_count']) {
+        // // console.log('visited');
+        item += '<div class="post read">';
+      } else {
+        item += '<div class="post">'
+      }
+      item += '<div id="' + post.id + '" class="item">\
+							<h2 class="grey">' + post.title + '</h2>\
+							<span class="description">' + post.description + '</span>\
+						</div>\
+					';
+      item += '</div>';
+      // <img src="' + post.img + '" width="107" height="60" />\  ;
+      // onclick="open_item(\'' + post.url + '\');"
+      //							<a href="' + post.url + '">\
+      //
+      // append data if not a refetch, if refetch, clear old data
+      if (refetch == true) {
+        // if this is a refetch
+        // popup is already open so we need to:
+        // clear the div the first time through
+        // then append data the rest of the cycles
+        // note: i was so tired when i did this
+        if (ii == 0) {
+          pinwheel = '<span class="label btn-primary">CURRENT</span>'
+          pp = $('#popup').html(pinwheel);
+          ii++
+        }
+
+        // replace data
+        p = $('#popup').append(item);
+
+      } else {
+        // for regular fetch
+        // popup is opening for the first time
+        // append data
+        p = $('#popup').append(item);
+      }
+
+    }
+    if (today_title == "delorean") {
+      $('#popup').append("<p>Error &#8734;: You Are A Muthafuckin' Time Traveller</p>");
+    }
+  });
+}
+
+function re_render_from_storage() {
+  chrome.storage.local.get(lunchInfo, function(storageResult) {
+    display_stories(storageResult);
+  });
+}
+
+function bindUiElementActions() {
+  document.getElementById("refetch").addEventListener('click', re_fetch);
+  
+}
+
+
+chrome.storage.onChanged.addListener(function(changes) {
+  console.log("change detected");
+  re_render_from_storage();
+});
+
+// when popup.html finishes loading, render 
+$(document).ready(function() {
+  re_render_from_storage();
+  re_fetch();
+});
